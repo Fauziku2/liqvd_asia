217f9af686784e684304cdf3964a2614e40e4985
Fauziku2 <M.fauzi_87@hotmail.com>
2017-01-18 06:13:36 +0800

    navbar completed

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  var jQuery = require('jquery')
+  require('jquery.easing')(jQuery)
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $('a.page-scroll').bind('click', function (event) {
+    var $anchor = $(this)
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo')
+    event.preventDefault()
+  })
+})
